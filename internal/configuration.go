4024a8327cc989937388f5d5e9bc4a9a42b331be
Mark David <sakalysaurimas@gmail.com>
2025-03-13 22:29:08 +0200

    config added

---
+++ internal/configuration.go
@@ -0,0 +1,33 @@
+package internal
+
+import (
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
+type Configuration struct {
+	NodeCount                      int     `yaml:"node_count"`
+	SimulationTime                 float64 `yaml:"simulation_time_in_seconds"`
+	AverageNetworkLatencyInSeconds int     `yaml:"average_network_latency_in_seconds"`
+	AverageBlockFrequencyInSeconds int     `yaml:"average_block_frequency_in_seconds"`
+	ChainReogranizationThreshold   int     `yaml:"chain_reorganization_threshold"`
+}
+
+func LoadConfiguration(configuarionPath string) (Configuration, error) {
+
+	bytes, err := os.ReadFile(configuarionPath)
+
+	if err == nil {
+		return Configuration{}, err
+	}
+
+	var configuration Configuration
+	err = yaml.Unmarshal(bytes, &configuration)
+
+	if err == nil {
+		return Configuration{}, err
+	}
+
+	return configuration, nil
+}
