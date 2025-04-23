fc3140de57b3c9202feaa77a4961ee41cd591d4a
Mark David <sakalysaurimas@gmail.com>
2025-04-23 17:17:13 +0300

    Log the wall-clock time of a run
    - Time each simulation run and print it, to watch performance while
      the scenarios grow.
    - Fix the graph labels.

--- cmd/simulation.go
+++ cmd/simulation.go
@@ -1,7 +1,10 @@
 package cmd
 
 import (
+	"time"
+
 	"github.com/MarkCDavid/burner/internal"
+	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
 
@@ -10,8 +13,11 @@ var simulationCmd = &cobra.Command{
 	RunE: func(cmd *cobra.Command, args []string) error {
 		// seed := int64(1745405314273631501)
 
+		start := time.Now()
 		internal.Simulate("./configuration/default.yaml", nil)
 		// internal.Simulate("./configuration/default.yaml", &seed)
+		elapsed := time.Since(start)
+		logrus.Warnf("Took %s", elapsed)
 		return nil
 	},
 }
